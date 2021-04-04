398fbee4a9fcf820136058cdb559bec5d70a272c
miyataken999 <miyataken999@gmail.com>
2021-04-04 19:36:23 +0000

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
       <footer className="py-5 bg-dark">
         <div className="container">
           <p className="m-0 text-center text-white">
-          これはこれでHTMLタグタグつかえれるといいsssssssssssssssssssssssssssssss
+          これはこれでHTMLタグタグつかえれるといいdddddddddddsssssssssssssssssssssssssssssss
             Built by <a href="https://www.accordbox.com/">kenichi miyata</a>
           </p>
         </div>
