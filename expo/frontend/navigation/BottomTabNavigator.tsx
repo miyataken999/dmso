3fda3c2cfa95e5a86442d90e7ad8f5636a61d8ce
miyataken999 <miyataken999@gmail.com>
2021-04-04 20:49:50 +0000

    sample

--- expo/frontend/navigation/BottomTabNavigator.tsx
+++ expo/frontend/navigation/BottomTabNavigator.tsx
@@ -52,7 +52,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab Onedddddddddddd Title' }}
+        options={{ headerTitle: 'Tab Title' }}
       />
     </TabOneStack.Navigator>
   );
